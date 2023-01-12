6465ef713ae1a3a1d866842a9725f37da1b35e00
Ssooob <121845110+Ssooob@users.noreply.github.com>
2023-01-12 17:53:58 +0900

    [Silver V] Title: 뒤집기, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/1439. 뒤집기/뒤집기.py
+++ 백준/Silver/1439. 뒤집기/뒤집기.py
@@ -2,15 +2,9 @@ numbers = list(map(int,input()))
 
 zero = []
 one = []
-new=[]
+cnt = 0
 for i in range(int(len(numbers))-1):
-    if numbers[i] != numbers[i+1] :    
-        # 0에서 1로 바뀐 경우
-        if numbers[i] == 0:
-            zero.append(i)
-        # 1에서 0으로 바뀐 경우
-        elif numbers[i] == 1:
-            one.append(i)
-#total = zero + one # 4 4
-#total.sort() # [1, 3, 5, 7, 9, 11, 13, 18]
-print(max(len(zero),len(one)))
+    if numbers[i] != numbers[i+1] :  
+        cnt += 1  
+
+print((cnt + 1) // 2)
