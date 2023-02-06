ca462a961517cdbcba351e1186f0398e9d691b12
Ssooob <121845110+Ssooob@users.noreply.github.com>
2023-02-06 17:08:46 +0900

    [Bronze I] Title: 행복한지 슬픈지, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10769. 행복한지 슬픈지/행복한지 슬픈지.py
+++ 백준/Bronze/10769. 행복한지 슬픈지/행복한지 슬픈지.py
@@ -1,21 +1,11 @@
 string = input()
-happy,sad,none = 0,0,0
-emotion = []
-cnt = 0
-for i in range(len(string)-2) :
-    if string[i] == ':' and string[i+1] == '-' :
-        if string[i+2] == ')':
-            happy += 1
-        elif string[i+2] == '(':
-            sad += 1
-if happy == 0 and sad == 0:
-    result = 'none'
-else :
-    if happy > sad :
-        result = 'happy'
-    elif sad > happy :
-        result = 'sad'
-    elif sad == happy :
-        result = 'unsure'
-print(result)
-        
+happy_cnt = string.count(':-)')
+sad_cnt = string.count(':-(')
+if happy_cnt > sad_cnt :
+    print('happy')
+elif happy_cnt < sad_cnt :
+    print('sad')
+elif happy_cnt == sad_cnt == 0 :
+    print('none')
+elif happy_cnt == sad_cnt :
+    print('unsure')
