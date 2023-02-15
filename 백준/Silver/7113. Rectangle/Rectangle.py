f517e7518245717fe94b6636f4349961a0362431
Ssooob <121845110+Ssooob@users.noreply.github.com>
2023-02-15 22:18:24 +0900

    [Silver V] Title: Rectangle, Time: 44 ms, Memory: 31412 KB -BaekjoonHub

---
+++ 백준/Silver/7113. Rectangle/Rectangle.py
@@ -0,0 +1,20 @@
+import sys
+sys.setrecursionlimit(10**7)
+a,b = map(int,input().split())
+cnt = 0 
+def rect_cut(a,b):
+    global cnt
+    if a == b :
+        cnt += 1
+        return cnt
+    else :
+        if a > b :
+            a -= b
+            cnt += 1
+            rect_cut(a,b)
+        elif a < b :
+            b -= a
+            cnt += 1
+            rect_cut(a,b)
+rect_cut(a,b)
+print(cnt)
