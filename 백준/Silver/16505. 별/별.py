3d235764188140255244fcbb111dba4a97d94cd7
Ssooob <121845110+Ssooob@users.noreply.github.com>
2023-02-16 19:17:33 +0900

    [Silver III] Title: 별, Time: 336 ms, Memory: 34404 KB -BaekjoonHub

---
+++ 백준/Silver/16505. 별/별.py
@@ -0,0 +1,39 @@
+import sys
+import math
+
+#sys.stdin=open('input.txt','r')
+
+def triangle(ans,n,m):
+    if n==1:
+        return '*'
+    else:
+        tmp=list(triangle(ans,n-1,m-1).split('\n'))
+        #print(tmp)
+        for i in range(len(tmp)):
+            ans+=tmp[i]*2
+            ans+=' '*(2**m-len(tmp[i]*2))
+            ans+='\n'
+
+
+        #ans=ans[:-1]
+        for i in range(len(tmp)-1):
+            ans+=tmp[i]
+            ans += ' ' * (2 ** m - len(tmp[i]))
+            ans+='\n'
+        ans+=tmp[-1]
+        ans+=' '*(2**m-len(tmp[-1]))
+        return ans
+
+if __name__=="__main__":
+    n=int(input())
+    ans=triangle('',n+1,n)
+    for i in range(0,2**(n*2),(2**n+1)):
+        ans1=ans[i:i+2**n]
+        start=-1
+        for s in range(len(ans1)-1,-1,-1):
+            if ans1[s]=='*':
+                start=s
+                break
+        for s in range(start+1):
+            print(ans1[s],end='')
+        print()
