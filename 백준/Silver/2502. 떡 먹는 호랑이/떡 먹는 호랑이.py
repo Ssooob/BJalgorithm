edf5bb50bb486ce350bb59d1d386263b4cc3ce60
Ssooob <121845110+Ssooob@users.noreply.github.com>
2023-03-16 19:26:27 +0900

    [Silver I] Title: 떡 먹는 호랑이, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/2502. 떡 먹는 호랑이/떡 먹는 호랑이.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+d, k = map(int, input().split())
+
+x = 1
+y = 0
+# 피보나치 구하기 
+for i in range(d-1):
+    x, y = y, x+y 
+    # y -> d번째 피보나치 수/ x -> d-1번째 피보나치 수
+for a in range(1, 100000): 
+    if (k-(x*a))%y == 0: # 나누어 떨어진다면, 몫이 b가 됨.
+        print(a, (k-(x*a))//y, sep="\n")
+        break
