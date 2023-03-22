816567c9c83a8197f4feccc11b1346d9f14d9cc9
Ssooob <121845110+Ssooob@users.noreply.github.com>
2023-03-23 05:11:13 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 56 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,23 +1,17 @@
 import sys
 input = sys.stdin.readline
-
 N = int(input())
 nums = list(map(int,input().split()))
-plus,minus,mul,div = map(int,input().split())
-
-res_max = -1e9
-res_min = 1e9
-
+op = list(map(int,input().split()))
+res_max,res_min = -1e9,1e9
 def dfs(depth,number,pl,mi,mu,di):
     global res_min, res_max
-
     if depth == N :
-        if number > res_max : # res_max = max(res_max,number)
+        if number > res_max :
          res_max = number
-        if number < res_min : # res_min = min(res_min,number)
+        if number < res_min : 
          res_min = number
          return
-
     if pl >= 1 :
         dfs(depth+1,number+nums[depth],pl-1,mi,mu,di)
     if mi >= 1 :
@@ -26,8 +20,6 @@ def dfs(depth,number,pl,mi,mu,di):
         dfs(depth+1,number*nums[depth],pl,mi,mu-1,di)
     if di >= 1 :
         dfs(depth+1,int(number/nums[depth]),pl,mi,mu,di-1)
-    
-dfs(1,nums[0],plus,minus,mul,div)
-
+dfs(1,nums[0],op[0],op[1],op[2],op[3])
 print(res_max)
 print(res_min)
