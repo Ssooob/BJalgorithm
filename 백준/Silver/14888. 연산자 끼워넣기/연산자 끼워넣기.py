a19312d8db5016285f7703e6cf560a6f23b6a7b5
Ssooob <121845110+Ssooob@users.noreply.github.com>
2023-03-23 05:04:02 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 64 ms, Memory: 31388 KB -BaekjoonHub

---
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -0,0 +1,29 @@
+N = int(input())
+nums = list(map(int,input().split()))
+plus,minus,mul,div = map(int,input().split())
+
+res_max = -1e9
+res_min = 1e9
+
+def dfs(depth,number,pl,mi,mu,di):
+    global res_min, res_max
+
+    # 숫자를 다 사용했으면 
+    if depth == N :
+         res_max = max(res_max,number)
+         res_min = min(res_min,number)
+         return
+
+    if pl >= 1 :
+        dfs(depth+1,number+nums[depth],pl-1,mi,mu,di)
+    if mi >= 1 :
+        dfs(depth+1,number-nums[depth],pl,mi-1,mu,di)
+    if mu >= 1 :
+        dfs(depth+1,number*nums[depth],pl,mi,mu-1,di)
+    if di >= 1 :
+        dfs(depth+1,int(number/nums[depth]),pl,mi,mu,di-1)
+    
+dfs(1,nums[0],plus,minus,mul,div)
+
+print(res_max)
+print(res_min)
