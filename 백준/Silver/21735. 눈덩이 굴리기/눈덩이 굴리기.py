a3ef302eae32f22e4ad2fbbc0a5580c0eb86537b
Ssooob <121845110+Ssooob@users.noreply.github.com>
2023-03-23 02:52:55 +0900

    [Silver III] Title: 눈덩이 굴리기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/21735. 눈덩이 굴리기/눈덩이 굴리기.py
+++ 백준/Silver/21735. 눈덩이 굴리기/눈덩이 굴리기.py
@@ -1,46 +1,16 @@
+import sys
+input = sys.stdin.readline
 N,M = map(int,input().split())
-snows = list(map(int,input().split()))
-
-# 컨셉.. 두 가지를 실행했을 때 더 크게 남는애를 선택하는게 낫지 않나? 
-# max(1안, 2안 해서) 더하기 
-# 모든 선택마다 
-
-def dfs(depth): # , snow_idx
-    global res, total, snows
-
-    # 만약 시간이 됐다면 각 경우의 수에 대한 total값을 도출
-    if depth == M : # or snow_idx == N-1: # 그리고 마지막꺼 골랐을 때 
-        snow_idx,snow_val = -1,1
-        for s in snow:
-            # 0 0 0 0 1 
-            if s == 0 :
-                if snow_idx +1 < N : 
-                    snow_idx += 1
-                    snow_val += snows[snow_idx]
-                    
-                else :
-                    break
-
-            elif s == 1 :
-                if snow_idx +2 < N : 
-                    div_snow = int(snow_val//2)
-                    snow_idx += 2
-                    snow_val =  div_snow + snows[snow_idx]
-                    
-                else :
-                    break
-        # print(snow,snow_val)
-        total.append(snow_val)
-
+snows = [0] + list(map(int,input().split()))
+res = -1 
+def dfs(idx, snow, time):
+    global res
+    if time < 0 :
         return
-
-    for i in range(2): # 0과 1의 경우에 수로 5번을 고를 거임 
-        snow.append(i)
-        dfs(depth+1) 
-        snow.pop()
-
-
-total,res = [],0
-snow = []
-dfs(0) # ,0
-print(max(total)) 
+    res = max(res,snow)
+    if idx +1 <= N :
+        dfs(idx+1,snow+snows[idx+1],time-1)
+    if idx +2 <= N :
+        dfs(idx+2,snow//2+snows[idx+2],time-1)
+dfs(0,1,M)
+print(res)
