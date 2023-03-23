86612d26afd00720a59711686fe0c426896a5421
Ssooob <121845110+Ssooob@users.noreply.github.com>
2023-03-23 20:05:25 +0900

    [Silver II] Title: 스타트와 링크, Time: 4792 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -1,29 +1,38 @@
-import sys
 N = int(input())
-board = [list(map(int,input().split())) for _ in range(N)]
-visited = [False for _ in range(N)]
-INF = 2147000000
-res = INF
+effort = [list(map(int,input().split()))for _ in range(N)]
+min_diff = 1e9
+s,l = [],[]
 
-# ✨ DFS
-def DFS(L,idx):
-    global res
-    if L == N//2:
-        A = 0
-        B = 0
-        for i in range(N):
-            for j in range(N):
-                if visited[i] and visited[j]:
-                    A += board[i][j]
-                elif not visited[i] and not visited[j]:
-                    B +=board[i][j]
-        res = min(res, abs(A-B))
-        return
-    for i in range(idx,N):
-        if not visited[i]:
-            visited[i] = True
-            DFS(L+1,i+1)
-            visited[i] = False
-            
-DFS(0,0)
-print(res)
+def dfs(depth, idx): # idx가 
+    global min_diff
+    # depth 기준 기반 팀 나누기   
+    if depth == N//2:
+        s_effort,l_effort = 0,0
+        # print(visited)
+        
+        # 능력치 구하기
+        for i in range(N-1):
+            for j in range(i+1,N):
+                if visited[i] == True and visited[j] == True:
+                    #print('stark',j,k)
+                    s_effort += effort[i][j]
+                    s_effort += effort[j][i]
+                elif visited[i] == False and visited[j] == False:
+                    #print('link',j,k)
+                    l_effort += effort[i][j]
+                    l_effort += effort[j][i]
+        #print(s_effort,l_effort,min_diff)
+        # 최소치 구하기 
+        min_diff = min(min_diff,abs(s_effort-l_effort))
+
+    # 중복 없는 수열을 구하기 위함  
+    for i in range(idx, N): # 그냥 1~N까지 돌면, 
+        if visited[i]:
+            continue
+        visited[i] = True
+        dfs(depth+1, i+1)
+        visited[i] = False
+        
+visited = [False]*(N)
+dfs(0,0)
+print(min_diff)
