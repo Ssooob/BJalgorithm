3c7a0f5465fb0f38d0d4c7f8aecfd0e148fcc1e5
Ssooob <121845110+Ssooob@users.noreply.github.com>
2023-03-30 03:07:17 +0900

    [Silver II] Title: 풍선 공장, Time: 1616 ms, Memory: 150704 KB -BaekjoonHub

--- 백준/Silver/15810. 풍선 공장/풍선 공장.py
+++ 백준/Silver/15810. 풍선 공장/풍선 공장.py
@@ -1,26 +1,12 @@
-import sys
-input = sys.stdin.readline
-
-def isPossible(target_time):
-    balloon=0
-    for staff in make_balloon:
-        balloon += target_time//staff
-    if balloon>=M:
-        return True
-    return False
-
-N,M = map(int,input().split())
-make_balloon = list(map(int,input().split()))
-
-start = 0
-end = 1e12
-answer = 0
-while start<=end:
-    mid = (start+end)//2
-    if isPossible(mid):
-        end = mid-1
-        answer=mid
+N, M = map(int, input().split())
+li = list(map(int, input().split()))
+s, e = 0, max(li)*M
+res = 0
+while s <= e:
+    m = (s+e)//2
+    if sum([m//n for n in li]) >= M:
+        res = m
+        e = m-1
     else:
-        start = mid+1
-
-print(int(answer))
+        s = m+1
+print(res)
