713ffd1d607e793a6a8a814c056c4c4456d712ec
Ssooob <121845110+Ssooob@users.noreply.github.com>
2023-04-05 22:41:12 +0900

    [Silver III] Title: N과 M (1), Time: 144 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,21 +1,17 @@
-def dfs():
-    
-    # 조건을 만족하면 해당 문을 return
-    if len(stack)== m : # 길이가 맞으면 
-        print(' '.join(map(str,stack))) # print
+def dfs(level):
+    if level == m:
+        print(' '.join(map(str,result)))
         return
-    
-    # 방문하지 않았을 경우에만 방문해주도록 
-    for i in range(1, n+1): # 1,2,3,4
-        if visited[i] :  #  visited가 True일 때
-            continue # 아래 코드를 실행하지 않고 건너뜀 
-        visited[i] = True # False라면 True로 바꾸고
-        stack.append(i) # stack에 하나 쌓고.. 
-        dfs() # 다시 호출
-        stack.pop() # 안에 있는거 지우고 
-        visited[i] = False # 다시 False로 만들기 (나머지 것들도 있으니까)
+    for i in range(1, n+1):
+        if visited[i] == True:
+            continue
+
+        result[level] = i
+        visited[i] = True
+        dfs(level + 1)
+        visited[i] = False
 
 n,m = map(int,input().split())
-stack = []
 visited = [False]*(n+1)
-dfs()
+result = [0] * m
+dfs(0)
