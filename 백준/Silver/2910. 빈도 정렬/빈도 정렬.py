e911c670ec00b6f58cb34989cdfcdede73655d07
Ssooob <121845110+Ssooob@users.noreply.github.com>
2023-04-14 16:14:20 +0900

    [Silver III] Title: 빈도 정렬, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2910. 빈도 정렬/빈도 정렬.py
+++ 백준/Silver/2910. 빈도 정렬/빈도 정렬.py
@@ -1,21 +1,14 @@
 n,c= map(int,input().split()) # n : 길이, c : 숫자는 모두 c보다 작거나 같다
 numbers = list(map(int,input().split()))
-
-res = []
-nn = []
-for i in range(len(numbers)):
-    if len(res) == 0 :
-        res.append([numbers[i],1,i])
-        nn.append(numbers[i]) 
+res = {}
+idx = 0 
+for i in numbers:
+    if i not in res:
+        res[i] = [1, idx] 
+        idx += 1
     else :
-        if numbers[i] not in nn:
-            res.append([numbers[i],1,i])
-            nn.append(numbers[i])
-        else :
-            for j in range(len(res)) :
-                if res[j][0] == numbers[i]:
-                    res[j][1] += 1
-res = sorted(res, key = lambda x : (-x[1],x[2]))
-for k in range(len(res)):
-    for kk in range(res[k][1]):
-        print(res[k][0], end = ' ')
+        res[i][0] += 1
+res = sorted(res.items(), key = lambda x : (-x[1][0],x[1][1]))
+for r in res :
+    for rr in range(r[1][0]):
+        print(r[0], end = ' ')
