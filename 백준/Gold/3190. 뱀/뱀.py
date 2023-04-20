dc14ef4ffa4d20de3506b61378664abd5c0db658
Ssooob <121845110+Ssooob@users.noreply.github.com>
2023-04-20 13:07:01 +0900

    [Gold IV] Title: 뱀, Time: 68 ms, Memory: 34176 KB -BaekjoonHub

---
+++ 백준/Gold/3190. 뱀/뱀.py
@@ -0,0 +1,49 @@
+import sys
+input = sys.stdin.readline
+from collections import deque
+n = int(input())
+k = int(input())
+graph = [[0] * n for _ in range(n)]
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
+for i in range(k):
+    a, b = map(int, input().split())
+    graph[a - 1][b - 1] = 2
+l = int(input())
+Dict = dict()
+queue = deque()
+queue.append((0, 0))
+for i in range(l):
+    x, c = input().split()
+    Dict[int(x)] = c
+x, y = 0, 0
+graph[x][y] = 1
+cnt = 0
+direction = 0
+def turn(d):
+    global direction
+    if d == 'L':
+        direction = (direction - 1) % 4
+    else:
+        direction = (direction + 1) % 4
+while True:
+    cnt += 1
+    x += dx[direction]
+    y += dy[direction]
+    if x < 0 or x >= n or y < 0 or y >= n:
+        break
+    if graph[x][y] == 2:
+        graph[x][y] = 1
+        queue.append((x, y))
+        if cnt in Dict:
+            turn(Dict[cnt])
+    elif graph[x][y] == 0:
+        graph[x][y] = 1
+        queue.append((x, y))
+        nx, ny = queue.popleft()
+        graph[nx][ny] = 0
+        if cnt in Dict:
+            turn(Dict[cnt])
+    else :
+        break
+print(cnt)
